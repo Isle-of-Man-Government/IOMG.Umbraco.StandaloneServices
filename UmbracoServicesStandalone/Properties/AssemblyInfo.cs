3af91bf7ee121ffcef0f1322dee650831da95990
braydie <braydie.grove@gmail.com>
2014-12-11 11:54:36 +0000

    Incremented version number in assembly info

--- UmbracoServicesStandalone/Properties/AssemblyInfo.cs
+++ UmbracoServicesStandalone/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Resources;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
 [assembly: NeutralResourcesLanguageAttribute("")]
